c2866a9356b372ee02e3f08df215023d4e0819e3
Benoit Baudaux <baudaux@yahoo.fr>
2023-06-02 20:02:45 +0200

    Continue fb

--- src/fb/src/fb.c
+++ src/fb/src/fb.c
@@ -26,6 +26,8 @@
 #include <time.h>
 #include <sys/timerfd.h>
 
+#include <linux/fb.h>
+
 #include "msg.h"
 
 #include <emscripten.h>
@@ -42,49 +44,6 @@
 static int major = 0;
 static int minor = 0;
 
-#define FBIOGET_VSCREENINFO	0x4600
-#define FBIOPUT_VSCREENINFO	0x4601
-#define FBIOGET_FSCREENINFO	0x4602
-#define FBIOGETCMAP		0x4604
-#define FBIOPUTCMAP		0x4605
-#define FBIOPAN_DISPLAY		0x4606
-
-struct fb_var_screeninfo {
-
-  uint32_t xres;			/* visible resolution		*/
-  uint32_t yres;
-  uint32_t xres_virtual;		/* virtual resolution		*/
-  uint32_t yres_virtual;
-  uint32_t xoffset;			/* offset from virtual to visible */
-  uint32_t yoffset;			/* resolution			*/
-
-  uint32_t bits_per_pixel;		/* guess what			*/
-  uint32_t grayscale;		/* 0 = color, 1 = grayscale,	*/
-					/* >1 = FOURCC			*/
-	
-};
-
-struct fb_fix_screeninfo {
-  
-  char id[16];			/* identification string eg "TT Builtin" */
-  unsigned long smem_start;	/* Start of frame buffer mem */
-  /* (physical address) */
-  uint32_t smem_len;			/* Length of frame buffer mem */
-  uint32_t type;			/* see FB_TYPE_*		*/
-  uint32_t type_aux;			/* Interleave for interleaved Planes */
-  uint32_t visual;			/* see FB_VISUAL_*		*/ 
-  uint16_t xpanstep;			/* zero if no hardware panning  */
-  uint16_t ypanstep;			/* zero if no hardware panning  */
-  uint16_t ywrapstep;		/* zero if no hardware ywrap    */
-  uint32_t line_length;		/* length of a line in bytes    */
-  unsigned long mmio_start;	/* Start of Memory Mapped I/O   */
-  /* (physical address) */
-  uint32_t mmio_len;			/* Length of Memory Mapped I/O  */
-  uint32_t accel;			/* Indicate to driver which	*/
-					/*  specific chip/card we have	*/
-  uint16_t capabilities;		/* see FB_CAP_*			*/
-  uint16_t reserved[2];		/* Reserved for future compatibility */
-};
 
 int main() {
 
@@ -224,12 +183,20 @@ int main() {
 	vinfo->xres = (uint32_t) scale*800;
 	vinfo->yres = (uint32_t) scale*600;
 	vinfo->bits_per_pixel = 32;
+	
 
 	msg->_errno = 0;
       }
       else if (msg->_u.ioctl_msg.op == FBIOGET_FSCREENINFO) {
 
-	struct fb_var_screeninfo * finfo = (struct fb_var_screeninfo *)msg->_u.ioctl_msg.buf;
+	struct fb_fix_screeninfo * finfo = (struct fb_fix_screeninfo *)msg->_u.ioctl_msg.buf;
+
+	double scale = EM_ASM_DOUBLE({
+
+	    return window.devicePixelRatio;
+	  });
+
+	finfo->line_length = (uint32_t) scale*800*4;
 
 	msg->_errno = 0;
 	
